f9e1d671757d8e51a7c81e45c99843ba38d40821
Jan Heuermann <jan@jotaen.net>
2016-09-06 17:24:35 +0200

    Create whole widget in one place

---
+++ src/distance.js
@@ -0,0 +1,44 @@
+'use strict'
+
+module.exports = (domNode, actualDistance, targetDistance) => {
+  // Setup Container
+  const container = document.createElement('div')
+  container.setAttribute('class', 'widget circle')
+  domNode.appendChild(container)
+
+  // Render Text
+  const value = document.createElement('div')
+  value.setAttribute('class', 'circle-value')
+  value.innerText = actualDistance.toFixed(1)
+  container.appendChild(value)
+
+  const label = document.createElement('div')
+  label.setAttribute('class', 'circle-label')
+  label.innerText = targetDistance.toFixed(1)
+  container.appendChild(label)
+
+  // Render SVG
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  svg.setAttribute('width', '100%')
+  svg.setAttribute('height', '100%')
+  container.appendChild(svg)
+
+  const scale = new Plottable.Scales.Linear()
+  const colorScale = new Plottable.Scales.InterpolatedColor()
+  colorScale.range(['#e74c3c', '#1abc9c'])
+  const data = [
+    Math.floor(actualDistance*10),
+    Math.floor((targetDistance-actualDistance)*10)
+  ]
+
+  const plot = new Plottable.Plots.Pie()
+    .addDataset(new Plottable.Dataset(data))
+    .sectorValue((value) => value, scale)
+    .innerRadius(120, scale)
+    .attr('fill', (value) => value, colorScale)
+    .renderTo(svg)
+
+  window.addEventListener('resize', () => {
+    plot.redraw()
+  })
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const distance = require('./distance')
+
+module.exports = {
+  distance
+}
