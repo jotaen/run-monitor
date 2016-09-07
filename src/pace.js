50a51d13378db2b61c6523530ce0c9314dbed5ae
Jan Heuermann <jan@jotaen.net>
2016-09-07 15:03:04 +0200

    Tweak layout of pace clock/widget

--- src/pace.js
+++ src/pace.js
@@ -8,15 +8,15 @@ const markerPosition = (actualPace, targetPace) => {
   return 90 * 2.5 * middle / scaledTargetPace
 }
 
-const tick = (degrees) => {
+const tick = (degrees, color) => {
   const marker = document.createElement('div')
   marker.setAttribute('style', [
     'position: absolute',
     'bottom: -1px',
-    'left: 55px',
+    'left: 54px',
     'height: 50px',
-    'width: 2px',
-    'border-top: 8px solid #bbb',
+    'width: 4px',
+    'border-top: 5px solid ' + color,
     'transform-origin: bottom',
     'transform: rotate(' + degrees + 'deg)'
   ].join(';'))
@@ -32,12 +32,12 @@ module.exports = (domNode, actualPace, targetPace) => {
   // Render Text
   const value = document.createElement('div')
   value.setAttribute('class', 'value pace-value')
-  value.innerText = actualPace.toFixed(2)
+  value.innerText = actualPace.toFixed(1)
   container.appendChild(value)
 
   const reference = document.createElement('div')
   reference.setAttribute('class', 'reference pace-reference')
-  reference.innerText = targetPace.toFixed(2)
+  reference.innerText = targetPace.toFixed(1)
   container.appendChild(reference)
 
   // Render Gauge
@@ -47,16 +47,16 @@ module.exports = (domNode, actualPace, targetPace) => {
     'height: 56px',
     'width: 112px',
     'border-radius: 90px 90px 0 0',
-    'border: 3px solid #bbb',
+    'border: 3px solid #95a5a6',
     'background: #e6e6e6'
   ].join(';'))
   container.appendChild(dashboard)
 
-  dashboard.appendChild(tick(-60))
-  dashboard.appendChild(tick(-30))
-  dashboard.appendChild(tick(0))
-  dashboard.appendChild(tick(30))
-  dashboard.appendChild(tick(60))
+  dashboard.appendChild(tick(-60, '#e74c3c'))
+  dashboard.appendChild(tick(-30, '#e67e22'))
+  dashboard.appendChild(tick(0, '#3498db'))
+  dashboard.appendChild(tick(30, '#2ecc71'))
+  dashboard.appendChild(tick(60, '#1abc9c'))
 
   const pointer = document.createElement('div')
   pointer.setAttribute('style', [
@@ -65,25 +65,38 @@ module.exports = (domNode, actualPace, targetPace) => {
     'right: 0',
     'bottom: -2px',
     'border-radius: 2px',
-    'background-color: #be643c',
-    'width: 3px',
+    'background-color: #2c3e50',
+    'width: 4px',
     'height: 52px',
     'transform-origin: bottom',
     'transform: rotate(' + Math.round(markerPosition(actualPace, targetPace)) + 'deg)'
   ].join(';'))
   dashboard.appendChild(pointer)
 
+  const dot = document.createElement('div')
+  dot.setAttribute('style', [
+    'position: absolute',
+    'left: -2px',
+    'right: 0',
+    'width: 8px',
+    'height: 8px',
+    'bottom: -3px',
+    'border-radius: 4px',
+    'background-color: #2c3e50'
+  ].join(';'))
+  pointer.appendChild(dot)
+
   const arrow = document.createElement('div')
   arrow.setAttribute('style', [
     'position: absolute',
-    'top: -6px',
+    'top: -5px',
     'left: 2px',
     'width: 0',
     'height: 0',
-    'margin-left: -8px',
-    'border-left: 8px solid transparent',
-    'border-right: 8px solid transparent',
-    'border-bottom: 16px solid #be643c'
+    'margin-left: -7px',
+    'border-left: 7px solid transparent',
+    'border-right: 7px solid transparent',
+    'border-bottom: 15px solid #2c3e50'
   ].join(';'))
   pointer.appendChild(arrow)
 }
