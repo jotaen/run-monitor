4de486a2fb0f965deb715c35ad832b64b6f7676f
Jan Heuermann <jan@jotaen.net>
2016-10-10 23:54:40 +0200

    Implement distance widget

---
+++ app/components/Distance/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+import React from 'react'
+import Label from '../label'
+import { fullPercent } from '../../lib/number'
+import { strokeDasharray } from '../../lib/styleProp'
+require('./style.scss')
+
+const Distance = ({actualValue, targetValue, estimatedValue}) => {
+  const relation = fullPercent(actualValue / targetValue)
+  return (
+    <div title='Distance' className='run__widget'>
+      <Label value={actualValue.toFixed(1)} element='distance' modifier='actual' />
+      <Label value={targetValue.toFixed(1)} element='distance' modifier='target' />
+      <svg viewBox='0 0 32 32' className='donut'>
+      <circle r='16' cx='16' cy='16' className='donut__ground' style={strokeDasharray(101)} />
+        <circle r='16' cx='16' cy='16' className='donut__fill' style={strokeDasharray(relation)} />
+      </svg>
+    </div>
+  )
+}
+
+Distance.propTypes = {
+  // actualValue: number greater than 0
+  actualValue: React.PropTypes.number.isRequired,
+  // targetValue: number greater than 0
+  targetValue: React.PropTypes.number.isRequired,
+}
+
+module.exports = Distance
--- app/components/Run/index.js
+++ app/components/Run/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import Heading from './heading'
+import Distance from '../distance'
 import Pace from '../pace'
 import Intensity from '../intensity'
 import Time from '../time'
@@ -10,6 +11,7 @@ require('./style.scss')
 const Run = ({run}) => (
   <div className='run'>
     <Heading date={run.date} route={run.route} />
+    <Distance actualValue={run.distance} targetValue={run.targetDistance} />
     <Pace actualValue={run.pace} targetValue={run.targetPace} />
     <Time actualValue={run.time} targetValue={run.targetTime} estimatedValue={run.estimatedTime} />
     <Intensity percentValue={run.intensity} />
